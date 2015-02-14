e8a8c0ca8c28e446578c7a70fd961cd386db9b6e
Matteo Sottana <s_matteo@tiscali.it>
2015-02-14 15:52:05 +0100

    Numero nodi = 4

--- src/catene2/Catene2.java
+++ src/catene2/Catene2.java
@@ -95,7 +95,7 @@ public class Catene2 {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        int n = 10;
+        int n = 5;
         long startTime = System.currentTimeMillis();
         long stopTime;
         long elapsedTime;
